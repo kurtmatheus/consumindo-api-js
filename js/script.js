a9ff0f310bc67913f5311002c9d9634d12cf5e96
kurtmatheus <kurtmatheus93@gmail.com>
2023-01-02 12:56:02 -0300

    Autopreenchimento e validação dinâmica

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
+const campoCEP = document.querySelector('#cep');
+const elementosEndereco = document.querySelectorAll('[data-endereco]');
+const divCep = document.querySelector('#cep_invalido');
+
+campoCEP.addEventListener('keyup', (evento) => {
+    evento.preventDefault();
+    const entradaUsuarioCep = evento.target.value;
+    if (entradaUsuarioCep.length == 8) {
+        buscaCEP(entradaUsuarioCep);
+    } else if(entradaUsuarioCep.length == 0) {
+        elementosEndereco.forEach(element => {
+            element.value = "";
+            divCep.style.display = 'none';     
+         });
+    }
+});
+
 async function buscaCEP(cep) {
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        consultaCEPjson = await consultaCEP.json();
+        var consultaCEPjson = await consultaCEP.json();
 
-        if(consultaCEPjson.erro){
-            throw Error('CEP inexistente!');
-        } 
-        
-        console.log(consultaCEPjson);
+        if(consultaCEPjson.erro) {
+            divCep.style.display = 'inline-block';            
+        } else { 
+            elementosEndereco.forEach(element => {
+               element.value = consultaCEPjson[element.id];
+            });
+        }
+
         
     } catch(erro) {
         console.log(erro)
@@ -15,4 +35,3 @@ async function buscaCEP(cep) {
 
 }
 
-buscaCEP('66033196')
